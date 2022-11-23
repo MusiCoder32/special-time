fb88845b005d19cf66f3b04df5ae2926cf2c3f1a
wyj <w327134@163.com>
2022-11-23 16:26:38 +0800

    解决iphone6无法登陆问题

--- common/appInit.js
+++ common/appInit.js
@@ -40,73 +40,73 @@ export default async function() {
 	db.on('error', onDBError)
 
 
-	//拦截云对象请求
-	uniCloud.interceptObject({
-		async invoke({
-			objectName, // 云对象名称
-			methodName, // 云对象的方法名称
-			params // 参数列表
-		}) {
-			// console.log('interceptObject',{
-			// 	objectName, // 云对象名称
-			// 	methodName, // 云对象的方法名称
-			// 	params // 参数列表
-			// });
-			if(objectName == "uni-id-co" && (methodName.includes('loginBy') ||  ['login','registerUser'].includes(methodName) )){
-				console.log('执行登录相关云对象');
-				params[0].inviteCode = await new Promise((callBack) => {
-					uni.getClipboardData({
-						success: function(res) {
-							console.log('剪切板内容：'+res.data);
-							if (res.data.slice(0, 18) == 'uniInvitationCode:') {
-								let uniInvitationCode = res.data.slice(18, 38)
-								console.log('当前用户是其他用户推荐下载的,推荐者的code是：' + uniInvitationCode);
-								// uni.showModal({
-								// 	content: '当前用户是其他用户推荐下载的,推荐者的code是：'+uniInvitationCode,
-								// 	showCancel: false
-								// });
-								callBack(uniInvitationCode)
-								//当前用户是其他用户推荐下载的。这里登记他的推荐者id 为当前用户的myInviteCode。判断如果是注册
-							} else {
-								callBack()
-							}
-						},
-						fail() {
-							console.log('error--');
-							callBack()
-						},
-						complete() {
-							// #ifdef MP-WEIXIN
-							uni.hideToast()
-							// #endif
-						}
-					});
-				})
-				// console.log(params);
-			}
-			// console.log(params);
-		},
-		success(e) {
-			console.log(e);
-		},
-		complete() {
-
-		},
-		fail(e){
-			console.error(e);
-			// if (debug) {
-			// 	uni.showModal({
-			// 		content: JSON.stringify(e),
-			// 		showCancel: false
-			// 	});
-			// }else{
-			// 	uni.showToast({
-			// 		title: '系统错误请稍后再试',
-			// 		icon:'error'
-			// 	});
-			// }
-		}
-	})
+	//拦截云对象请求 iphone6会造成无法登陆
+	// uniCloud.interceptObject({
+	// 	async invoke({
+	// 		objectName, // 云对象名称
+	// 		methodName, // 云对象的方法名称
+	// 		params // 参数列表
+	// 	}) {
+	// 		// console.log('interceptObject',{
+	// 		// 	objectName, // 云对象名称
+	// 		// 	methodName, // 云对象的方法名称
+	// 		// 	params // 参数列表
+	// 		// });
+	// 		if(objectName == "uni-id-co" && (methodName.includes('loginBy') ||  ['login','registerUser'].includes(methodName) )){
+	// 			console.log('执行登录相关云对象');
+	// 			params[0].inviteCode = await new Promise((callBack) => {
+	// 				uni.getClipboardData({
+	// 					success: function(res) {
+	// 						console.log('剪切板内容：'+res.data);
+	// 						if (res.data.slice(0, 18) == 'uniInvitationCode:') {
+	// 							let uniInvitationCode = res.data.slice(18, 38)
+	// 							console.log('当前用户是其他用户推荐下载的,推荐者的code是：' + uniInvitationCode);
+	// 							// uni.showModal({
+	// 							// 	content: '当前用户是其他用户推荐下载的,推荐者的code是：'+uniInvitationCode,
+	// 							// 	showCancel: false
+	// 							// });
+	// 							callBack(uniInvitationCode)
+	// 							//当前用户是其他用户推荐下载的。这里登记他的推荐者id 为当前用户的myInviteCode。判断如果是注册
+	// 						} else {
+	// 							callBack()
+	// 						}
+	// 					},
+	// 					fail() {
+	// 						console.log('error--');
+	// 						callBack()
+	// 					},
+	// 					complete() {
+	// 						// #ifdef MP-WEIXIN
+	// 						uni.hideToast()
+	// 						// #endif
+	// 					}
+	// 				});
+	// 			})
+	// 			// console.log(params);
+	// 		}
+	// 		// console.log(params);
+	// 	},
+	// 	success(e) {
+	// 		console.log(e);
+	// 	},
+	// 	complete() {
+	//
+	// 	},
+	// 	fail(e){
+	// 		console.error(e);
+	// 		// if (debug) {
+	// 		// 	uni.showModal({
+	// 		// 		content: JSON.stringify(e),
+	// 		// 		showCancel: false
+	// 		// 	});
+	// 		// }else{
+	// 		// 	uni.showToast({
+	// 		// 		title: '系统错误请稍后再试',
+	// 		// 		icon:'error'
+	// 		// 	});
+	// 		// }
+	// 	}
+	// })
 
 
 	// #ifdef APP-PLUS
