a6ccf7a371bd8158df4c174f87288ab2b754d172
wyj <w327134@163.com>
2023-06-04 00:01:11 +0800

    修改时光广场日期分类描述

--- uniCloud-aliyun/cloudfunctions/time-ground-category/index.js
+++ uniCloud-aliyun/cloudfunctions/time-ground-category/index.js
@@ -1,7 +1,7 @@
 'use strict'
 exports.main = async (event, context) => {
     //event为客户端上传的参数
-    const category = ['明星相关','热点日期', '传统节日','著名人物', '国际节日']
+    const category = ['明星相关', '传统节日','著名人物', '国际节日','其他日期']
 
     //返回数据给客户端
     return category
