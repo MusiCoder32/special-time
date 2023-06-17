45fc54ffa08cfd62aae3c538eaf89eeaf1ed84b2
wyj <w327134@163.com>
2023-06-17 13:58:57 +0800

    添加版本更新检测

--- App.vue
+++ App.vue
@@ -8,6 +8,32 @@ import uniIdPageInit from '@/uni_modules/uni-id-pages/init.js'
 import { saveSceneId } from './utils/common'
 uni.$navStatusHeight = 0
 
+function update() {
+    const updateManager = wx.getUpdateManager()
+
+    updateManager.onCheckForUpdate(function (res) {
+        // 请求完新版本信息的回调
+        console.log(res.hasUpdate)
+    })
+
+    updateManager.onUpdateReady(function () {
+        wx.showModal({
+            title: '更新提示',
+            content: '新版本已经准备好，是否重启应用？',
+            success: function (res) {
+                if (res.confirm) {
+                    // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                    updateManager.applyUpdate()
+                }
+            },
+        })
+    })
+
+    updateManager.onUpdateFailed(function () {
+        // 新版本下载失败
+    })
+}
+
 export default {
     globalData: {
         pushClientId: '',
@@ -24,6 +50,9 @@ export default {
         $t: {},
     },
     onLaunch: async function (e) {
+        // #ifdef MP-WEIXIN
+        update() //检查小程序版本
+        // #endif
         console.log(e)
         console.log('App Launch')
         /**
@@ -41,19 +70,19 @@ export default {
             })
         }
 
-        uni.onPushMessage((res) => {
-            console.log('收到推送消息：', res) //监听推送消息
-        })
-        // uni-app客户端获取push客户端标记
-        uni.getPushClientId({
-            success: (res) => {
-                console.log('客户端推送标识:', res.cid)
-                this.globalData.pushClientId = res.cid
-            },
-            fail(err) {
-                console.log(err)
-            },
-        })
+        // uni.onPushMessage((res) => {
+        //     console.log('收到推送消息：', res) //监听推送消息
+        // })
+        // // uni-app客户端获取push客户端标记
+        // uni.getPushClientId({
+        //     success: (res) => {
+        //         console.log('客户端推送标识:', res.cid)
+        //         this.globalData.pushClientId = res.cid
+        //     },
+        //     fail(err) {
+        //         console.log(err)
+        //     },
+        // })
         this.globalData.$i18n = this.$i18n
         this.globalData.$t = (str) => this.$t(str)
         const navBarHeight = 44
