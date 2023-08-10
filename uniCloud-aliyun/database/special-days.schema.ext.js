8cca0c7d896bb40bde1d6a7763ea4463909732c2
wyj <w327134@163.com>
2023-08-10 10:34:14 +0800

    解决bsonTypew传null时无法置空对应字段问题

--- uniCloud-aliyun/database/special-days.schema.ext.js
+++ uniCloud-aliyun/database/special-days.schema.ext.js
@@ -1,10 +1,20 @@
-// article.schema.ext.js
 module.exports = {
     trigger: {
-        beforeUpdate: async function ({ collection, operation, where, updateData, clientInfo } = {}) {
-            const id = where && where._id
-            if (typeof id === 'string') {
+        beforeUpdate: async function({
+            collection,
+            operation,
+            docId,
+            updateData,
+            clientInfo
+        } = {}) {
+            if (typeof docId === 'string') {
                 updateData.update_date = Date.now() // 更新数据的update_date字段赋值为当前服务器时间
+                if (!updateData.avatar) {
+                    const db = uniCloud.database()
+                    db.collection('special-days').doc(docId).update({
+                        avatar: db.command.remove()
+                    })
+                }
             }
         },
     },
