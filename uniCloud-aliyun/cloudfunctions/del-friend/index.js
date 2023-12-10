526c08d9f93e9d33675108b4d7ce378c8c84a06f
wyj <w327134@163.com>
2023-12-10 09:02:45 +0800

    保存云函数删除数据库示例

--- uniCloud-aliyun/cloudfunctions/del-friend/index.js
+++ uniCloud-aliyun/cloudfunctions/del-friend/index.js
@@ -3,9 +3,17 @@ const db = uniCloud.database();
 
 exports.main = async (event, context) => {
     //event为客户端上传的参数
-    console.log('event : ', event)
-    const { _id } = JSON.parse(event.body)
-    const res = await db.collection('start-friends').doc(_id).remove()
+const res = await db.collection('special-days-share').where({
+	poster:[]
+}).remove()
+
+const ids = [12341234,34123423]
+	const res = await db.collection('start-friends').where({
+		lemmaId:{
+			$in:ids
+		}
+	}).remove()
+console.log(res);
 
     //返回数据给客户端
     return res
