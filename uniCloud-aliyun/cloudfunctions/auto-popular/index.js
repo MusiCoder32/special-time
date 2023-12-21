82f054ee6c6965feb47b1c80a9575771bfcfce89
wyj <w327134@163.com>
2023-12-21 09:18:13 +0800

    完成按照日期匹配出当天生日的明星

--- uniCloud-aliyun/cloudfunctions/auto-popular/index.js
+++ uniCloud-aliyun/cloudfunctions/auto-popular/index.js
@@ -3,26 +3,41 @@ const db = uniCloud.database();
 const collection = db.collection('special-days-share')
 const $ = db.command.aggregate
 
+
 exports.main = async (event, context) => {
+    try {
+        const res = await collection
+            .aggregate()
+            .addFields({
+                formattedDate: {
+                    $dateToString: {
+                        format: '%m-%d',
+                        date: {
+                            $add: [new Date('1970-01-01'), '$time'], // 假设你的时间戳字段为 timestampField
+                        },
+                    },
+                },
+            })
+            .match({
+                formattedDate: '01-01', // 替换成你需要的日期
+            })
+              .project({
+                _id: 1, // 显示默认的 _id 字段
+                formattedDate: 1, // 显示 formattedDate 字段
+                name: 1, // 显示其他需要的字段，如果有的话
+              })
+            .end();
 
-    const res = await collection
-        .aggregate()
-        .project({
-            formatDate: $.dayOfMonth('$time')
-        })
-        .limit(1)
-        .end()
-
-    // const res = await collection.where({
-    // 		lunar:0,
-    //         time:
-    // 	}).field({
-    //         time:true,
-    //         lunar:true,
-    //         name:true,
-    //     }).limit(1).get()
-    console.log(res);
-
-    //返回数据给客户端
-    return res
+        return {
+            code: 200,
+            data: res.data,
+            msg: '查询成功',
+        };
+    } catch (error) {
+        return {
+            code: 500,
+            data: null,
+            msg: '查询失败：' + error.message,
+        };
+    }
 };
