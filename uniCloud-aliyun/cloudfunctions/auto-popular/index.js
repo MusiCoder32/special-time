c98611fd28299273088db8c78735f66cabd6762c
wyj <w327134@163.com>
2023-12-27 18:17:01 +0800

    完成小红书access_token获取

--- uniCloud-aliyun/cloudfunctions/auto-popular/index.js
+++ uniCloud-aliyun/cloudfunctions/auto-popular/index.js
@@ -14,33 +14,38 @@ const appSecret = 'b1b497f11eb7e1eaea101f3d5eaeef01'
 
 const bookApi = {
     async getAccessToken() {
-        const timestamp = new Date().getTime()
-        const nonce = Math.random()
-        const expires_in = timestamp + 12 * 60 * 60 * 1000
-        const signature = sha256(qs.stringify({
-            nonce,
-            timeStamp:timestamp,
+        const timestamp = new Date().getTime() - 1000
+        const nonce = Math.random() + ''
+        const expires_in = timestamp + 24 * 60 * 60 * 1000
+        const str = qs.stringify({
             appKey,
-            appSecret,
-        }))
-        console.log(signature)
-        const res = await axios({
-            method: 'POST',
-            url: 'https://edith.xiaohongshu.com/api/sns/v1/ext/access/token',
-            data: JSON.stringify({
-                app_key:appKey,
-                nonce,
-                timestamp,
-                signature,
-                expires_in
-            }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        console.log(222,res)
+            nonce,
+            timeStamp: timestamp + '',
+        }) + appSecret
 
-    }
+        const signature = sha256(str)
+
+        try {
+            const res = await axios({
+                method: 'POST',
+                url: 'https://edith.xiaohongshu.com/api/sns/v1/ext/access/token',
+                data: {
+                    app_key: appKey,
+                    nonce,
+                    timestamp,
+                    signature,
+                    expires_in
+                },
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            return res.data.data.access_token
+        } catch (e) {
+            console.log(e);
+        }
+
+    },
 }
 
 
@@ -73,7 +78,7 @@ async function getBirthdays() {
                 name: 1, // 显示其他需要的字段，如果有的话
             })
             .end();
-    
+
         return {
             code: 200,
             data: res.data,
@@ -89,7 +94,7 @@ async function getBirthdays() {
 }
 
 exports.main = async (event, context) => {
-const res = await bookApi.getAccessToken()
-console.log(res);
-return {}
+    const res = await bookApi.getAccessToken()
+    console.log(res);
+    return {}
 };
